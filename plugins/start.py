0dfde84ceb0daae090e7069f5418e73305c75b8e
rohit870891 <koyeb77user@gmail.com>
2024-09-18 22:14:17 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -90,7 +90,7 @@ async def start_command(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton("⚡️ ᴀʙᴏᴜᴛ", callback_data = "about"),
-                    InlineKeyboardButton('🍁 sᴇʀɪᴇsғʟɪx', url='https://t.me/rohit_1888')
+                    InlineKeyboardButton('🍁 OWNER', url='https://t.me/rohit_1888')
                 ]
             ]
         )
