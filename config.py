fdf0eb7b1b7945fb79233110dcbbd6762aa054b3
rohit870891 <koyeb77user@gmail.com>
2024-11-22 22:19:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,6 +32,9 @@ PORT = os.environ.get("PORT", "8007")
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://nsdh77ygz5:b0zdFJkd9frHADgT@cluster0.vvof2.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
+#Time in seconds for message delete, put 0 to never delete
+TIME = int(os.environ.get("TIME", "60"))
+
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL_1 = int(os.environ.get("FORCE_SUB_CHANNEL_1", "-1002215102799"))
